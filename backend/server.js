04cadb27b9d6c2a4fbce92bb97e9ca09f720f598
codewilson64 <wilsongambit@gmail.com>
2025-04-03 11:27:18 +0700

    Add some code for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
@@ -5,16 +6,19 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { v2 as cloudinary } from 'cloudinary'
 
+// For deployment
+
 dotenv.config()
 
 const app = express()
+
+const __dirname = path.resolve()
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 })
-
 
 // middleware
 app.use(express.json({limit: '5mb'}))
@@ -29,6 +33,13 @@ import postRoutes from './routes/post.routes.js'
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/post', postRoutes)
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
+}
 
 // mongoose
 mongoose.connect(process.env.MONGO_URI)
